8ca583d0ebb38304a3d1cf00e559bc124cf498e3
DRation1 <128318816+DRation1@users.noreply.github.com>
2023-05-18 02:34:40 +0900

    Update CarRaceWIP.c

--- Week 11 Assignment/CarRaceWIP.c
+++ Week 11 Assignment/CarRaceWIP.c
@@ -3,11 +3,11 @@
 #include <stdlib.h> //표준 유틸리티 함수 헤더
 #include <conio.h> //콘솔 입출력 함수 헤더
 
-void dispCar(int carNo, int distance)
+void dispCar(int carNo, int distance) //별 출력 함수
 {
-    int i;
-    printf("CAR #%d:", carNo);
-    for(i = 0; i < distance/10; i++)
+    int i; //for 반복문 변수
+    printf("CAR #%d:", carNo); //자동차 번호를 출력 
+    for(i = 0; i < distance/10; i++) //main에서부터 받은 "거리"값을 기반으로 별 갯수 출력
     {
         printf("*");
     }
@@ -17,15 +17,15 @@ void dispCar(int carNo, int distance)
 int main()
 {
     int i;
-    int car1Dis = 0, car2Dis = 0;
-    srand((unsigned)time(NULL));
-    for(i=0; i < 6; i++)
+    int car1Dis = 0, car2Dis = 0; //각 "자동차"의 주행거리를 저장할 변수
+    srand((unsigned)time(NULL)); //시드값 생성
+    for(i=0; i < 6; i++) //6번 반복되는 for반복문
     {
-        car1Dis += rand() % 100;
+        car1Dis += rand() % 100; //각 자동차의 주행거리를 난수 생성기로 0부터99까지의 숫자를 생성하고 주행거리 변수에 할당
         car2Dis += rand() % 100;
-        dispCar(1, car1Dis);
+        dispCar(1, car1Dis); //각 자동차의 번호와 주행거리를 별을 출력하는 함수에 전달
         dispCar(2, car2Dis);
-        printf("------------\n");
-        _getch();
+        printf("------------\n"); 
+        _getch(); //키 입력시에 반복문이 다시 반복되도록
     }
 }
