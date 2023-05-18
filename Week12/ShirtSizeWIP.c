d5ce24cac127cb6854ef2aceb71caabbd06849eb
DRation1 <128318816+DRation1@users.noreply.github.com>
2023-05-18 19:01:25 +0900

    Update ShirtSizeWIP.c

--- Week12/ShirtSizeWIP.c
+++ Week12/ShirtSizeWIP.c
@@ -5,7 +5,7 @@ int solution(char shirtSize[], int shirtSizeLen)
     int* answer = (int*)malloc(sizeof(int)*6);
     for(int i = 0; i < shirtSizeLen; i++)
     {
-        if(shirtSize[i] == 'XS')
+        if(shirtSize[i] == 'XS') //strcmp 사용, 비교 후, answer 배열에 해당되는 인덱스 증감
         {
             answer[i] += 1;
         }
